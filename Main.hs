7639e132286d76303c31b4dc988aa6efe8586ce0
tristangreeno <greeno.tristan@gmail.com>
2018-03-05 13:29:14 -0600

    init

---
+++ Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import Test.QuickCheck
+
+prop_revapp :: [Int] -> [Int] -> Bool
+prop_revapp xs ys = reverse (xs++ys) == reverse xs ++ reverse ys
+
+main :: IO ()
+main = quickCheck prop_revapp
